30866061a4cea56a135cb041a6a0f37e1e18aa0e
Bohdan Andriychuk <dan.anddy.real@gmail.com>
2021-05-29 11:53:01 +0300

    Initial Commit

---
+++ cursova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cursova
+//
+//  Created by Bohdan Andriychuk on 29.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
